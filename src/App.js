643b8af9de4da7de226b3b7d0f7991079f03ab3c
Kobe Li <likobe23@gmail.com>
2021-08-08 13:41:32 -0700

    added inputs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="mt-4">
+      <Row>
+        <Col lg={6}>
+          <Form>
+            <Form.Group className="mb-3" controlId="primos-amt">
+              <Form.Label>How many Primogems do you have?</Form.Label>
+              <Form.Control type="number" min={0}></Form.Control>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="fates-amt">
+              <Form.Label>How many Fates do you have?</Form.Label>
+              <Form.Control type="number" min={0}></Form.Control>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="pulls-amt">
+              <Form.Label>How many pulls have you already done?</Form.Label>
+              <Form.Control type="number" min={0}></Form.Control>
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+    
   );
 }
 
