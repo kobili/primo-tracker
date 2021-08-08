74970316b66d2f6764b12e23490ace3b97ede35a
Kobe Li <likobe23@gmail.com>
2021-08-08 15:10:25 -0700

    made use of local storage to repopulate previously input values

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
@@ -10,15 +10,32 @@ import Col from 'react-bootstrap/Col';
 
 function App() {
 
-  const [primos, setPrimos] = useState(0);
-  const [fates, setFates] = useState(0);
-  const [prevPulls, setPrevPulls] = useState(0);
+  // state hooks
+  const [primos, setPrimos] = useState( parseInt(localStorage.getItem("primos")) || parseInt(0));
+  const [fates, setFates] = useState( parseInt(localStorage.getItem("fates")) || parseInt(0));
+  const [prevPulls, setPrevPulls] = useState( parseInt(localStorage.getItem("prevPulls")) || parseInt(0));
   const [totalPulls, setTotalPulls] = useState(0);
   const [numberOfHardPities, setNumberOfHardPities] = useState(0);
   const [primosToNextHardPity, setPrimosToNextHardPity] = useState(14400);
 
-  // newValue is a number
-  const updatePrimos = (newValue) => {
+  // useEffect to run a function on page load
+  useEffect(() => {
+    // set the total pulls and hard pity state variables from saved values
+    updateTotalPulls(primos, fates, prevPulls);
+  });
+
+  // use useEffect to save values to local storage when ever values are updated
+  useEffect(() => {
+      localStorage.setItem("primos", primos);
+      localStorage.setItem("fates", fates);
+      localStorage.setItem("prevPulls", prevPulls);
+      // localStorage.setItem("totalPulls", totalPulls);
+      // localStorage.setItem("numberOfHardPities", numberOfHardPities);
+      // localStorage.setItem("primosToNextHardPity", primosToNextHardPity);
+  }, [primos, fates, prevPulls]);
+
+  // functions to update state
+  const updatePrimos = (newValue) => {  // newValue is a number
     setPrimos(newValue);
     updateTotalPulls(newValue, fates, prevPulls);
   };
@@ -52,21 +69,21 @@ function App() {
           <Form>
             <Form.Group className="mb-3" controlId="primos-amt">
               <Form.Label>How many Primogems do you have?</Form.Label>
-              <Form.Control type="number" min={0} onChange={(e) => updatePrimos(parseInt(e.target.value))}></Form.Control>
+              <Form.Control type="number" min={0} onChange={(e) => updatePrimos(parseInt(e.target.value))} value={primos !== 0 ? primos : ""}></Form.Control>
             </Form.Group>
             <Form.Group className="mb-3" controlId="fates-amt">
               <Form.Label>How many Fates do you have?</Form.Label>
-              <Form.Control type="number" min={0} onChange={(e) => updateFates(parseInt(e.target.value))}></Form.Control>
+              <Form.Control type="number" min={0} onChange={(e) => updateFates(parseInt(e.target.value))} value={fates !== 0 ? fates : ""}></Form.Control>
             </Form.Group>
             <Form.Group className="mb-3" controlId="prev-pulls-amt">
               <Form.Label>How many pulls have you already done?</Form.Label>
-              <Form.Control type="number" min={0} onChange={(e) => updatePrevPulls(parseInt(e.target.value))}></Form.Control>
+              <Form.Control type="number" min={0} onChange={(e) => updatePrevPulls(parseInt(e.target.value))} value={prevPulls !== 0 ? prevPulls : ""}></Form.Control>
             </Form.Group>
           </Form>
           <div>Total Pulls on Banner: {Math.floor(totalPulls)}</div>
         </Col>
         <Col lg={6} className="align-content-center">
-          <div>You can hit hard pity {numberOfHardPities} {numberOfHardPities == 1 ? 'time' : 'times'}</div>
+          <div>You can hit hard pity {numberOfHardPities} {numberOfHardPities === 1 ? 'time' : 'times'}</div>
           <div>You need {primosToNextHardPity} primos to reach your next hard pity</div>
         </Col>
 
