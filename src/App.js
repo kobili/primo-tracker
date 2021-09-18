75b5da8909d7c258008a2167a37dee9e8a337555
Kobe Li <likobe23@gmail.com>
2021-09-18 00:54:21 -0700

    changed the title of the page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs'
 
 import PrimogemCalc from './components/PrimogemCalc';
 import TalentMatTracker from './components/TalentMatTracker';
 
 function App() {
   return (
-    <>
-      <PrimogemCalc />
-      <TalentMatTracker />
-    </>
+    // <>
+    //   <PrimogemCalc />
+    //   <TalentMatTracker />
+    // </>
+
+    <Tabs defaultActiveKey="primogem-calc" id="application-tab" className="mb-3">
+      <Tab eventKey="primogem-calc" title="Primogem Tracker">
+        <PrimogemCalc />
+      </Tab>
+      <Tab eventKey="talent-mat-tracker" title="Talent Material Tracker">
+        <TalentMatTracker />
+      </Tab>
+    </Tabs>
   );
 }
 
