a3faaa3962c9634c476f217f336a6cbf91001625
Kobe Li <likobe23@gmail.com>
2022-10-30 19:45:29 -0700

    Fetch from redux store for primogem input

--- src/components/currencyData/PrimogemInput.tsx
+++ src/components/currencyData/PrimogemInput.tsx
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 import primogemIcon from '../../icons/primogem.png';
 import { InputContainer, InputField, Image, Input, Button } from '../SharedComponents';
 
+import { useSelector, useDispatch } from 'react-redux';
+import { selectPrimogems, setPrimogems } from '../../store/slices/bannerDataSlice';
+
 const ButtonGroup = styled.div`
     display: flex;
     gap: 5px;
@@ -10,26 +13,30 @@ const ButtonGroup = styled.div`
 
 export const PrimogemInput = () => {
 
+    const dispatch = useDispatch();
+
     const [shouldShowOptions, setShouldShowOptions] = useState(false);
+
+    const primos = useSelector(selectPrimogems);
 
     return (
         <InputContainer>
             <InputField>
                 <Image src={primogemIcon} />
-                <Input type="number" />
+                <Input type="number" min={0} onChange={(e) => dispatch(setPrimogems(parseInt(e.target.value)))} value={primos !== 0 ? primos : ""}/>
             </InputField>
             <ButtonGroup>
-                <Button>+60</Button>
-                <Button>+90</Button>
+                <Button onClick={() => dispatch(setPrimogems(primos + 60))}>+60</Button>
+                <Button onClick={() => dispatch(setPrimogems(primos + 90))}>+90</Button>
                 <Button style={{ marginRight: '10px' }} onClick={() => setShouldShowOptions(!shouldShowOptions)}>{shouldShowOptions ? '<' : '>'}</Button>
             </ButtonGroup>
 
             {shouldShowOptions && <ButtonGroup>
-                <Button>+1</Button>
-                <Button>+2</Button>
-                <Button>+5</Button>
-                <Button>+10</Button>
-                <Button>+20</Button>
+                <Button onClick={() => dispatch(setPrimogems(primos + 1))}>+1</Button>
+                <Button onClick={() => dispatch(setPrimogems(primos + 2))}>+2</Button>
+                <Button onClick={() => dispatch(setPrimogems(primos + 5))}>+5</Button>
+                <Button onClick={() => dispatch(setPrimogems(primos + 10))}>+10</Button>
+                <Button onClick={() => dispatch(setPrimogems(primos + 20))}>+20</Button>
             </ButtonGroup>}
         </InputContainer>
     );
