753e81f32a4f444957fca379e134c1f977cabdf7
Kobe Li <likobe23@gmail.com>
2023-05-08 23:54:51 -0700

    Change shared divs to use tailwind

--- src/components/SharedComponents.tsx
+++ src/components/SharedComponents.tsx
@@ -1,23 +1,42 @@
 import styled from 'styled-components';
+import { ReactNode } from 'react';
 
 const INPUT_AND_BUTTON_HEIGHT = "36px";
 
-export const InputContainer = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 10px;
-    width: 100%;
-    margin-bottom: 20px`;
+interface DivProps  {
+    children: ReactNode[];
+}
 
-export const InputField = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;`;
+export const InputContainer = ({children}: DivProps) => {
+    return (
+        <div className='flex items-center gap-2 w-full mb-5'>
+            {children}
+        </div>
+    )
+}
+
+export const InputField = ({children}: DivProps) => {
+    return (
+        <div className='flex justify-center items-center'>
+            {children}
+        </div>
+    )
+}
+
+interface ImageProps {
+    src: string;
+}
 
 export const Image = styled.img`
     height: 38px;
     width: auto;
     margin-right: -50;`;
+
+// export const Image = ({src}: ImageProps) => {
+//     return (
+//         <img className='h-9 w-auto, -mr-12' src={src} />
+//     )
+// }
 
 export const Input = styled.input`
     max-width: 500px;
